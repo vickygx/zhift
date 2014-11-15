8e959597d07a5fc1bc65f30fd24aa38dc268da94
Vicky Gong <vickyg@mit.edu>
2014-11-14 22:12:42 -0500

    Adding basic shift controller, service, directive

---
+++ Zhift/public/javascripts/directives/shifts-directive.js
@@ -0,0 +1,17 @@
+var ZhiftApp = angular.module('ZhiftApp');
+
+
+/*  EXAMPLE OF DIRECTIVE
+*/
+ZhiftApp.directive('getAllShifts', ['ShiftService', function(ShiftService){
+  return {
+    restrict: "C", 
+    link: function(scope, element, attrs) {
+      element.bind("click", function(e){
+        scheduleId = 'temp';
+        ShiftService.displayAllShifts(scheduleId);
+      });
+    }
+  }
+}]);
+
