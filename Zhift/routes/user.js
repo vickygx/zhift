294c93d6a313e8a966337d61a609ff436b43ab4c
Dylan Joss <djoss@mit.edu>
2014-11-17 11:26:28 -0500

    skeleton for user API

--- Zhift/routes/user.js
+++ Zhift/routes/user.js
@@ -11,9 +11,54 @@ var OrgController = require('../controllers/organization');
 var errors = require('../errors/errors');
 var errorChecking = require('../errors/error-checking');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-    res.send('respond with a resource');
+/* POST request to create new manager */
+router.post('/manager', function(req, res) {
+
+});
+
+/* POST request to create new employee */
+router.post('/employee', function(req, res) {
+
+});
+
+/* GET the manager associated with the id */
+router.get('/manager/:id', function(req, res) {
+
+});
+
+/* GET the manager associated with the id */
+router.get('/employee/:id', function(req, res) {
+
+});
+
+/* PUT to change the password of the given manager */
+router.put('/manager/:id', function(req, res) {
+
+});
+
+/* PUT to change the password of the given employee */
+router.put('/employee/:id', function(req, res) {
+
+});
+
+/* GET all managers associated with the org with the id */
+router.get('/org/:id/manager', function(req, res) {
+
+});
+
+/* GET all employees associated with the org with the id */
+router.get('/org/:id/employee', function(req, res) {
+
+});
+
+/* DELETE the user with the id */
+router.delete('/:id', function(req, res) {
+
+});
+
+/* GET all employees associated with the schedule with the id */
+router.get('/sched/:id', function(req, res) {
+
 });
 
 module.exports = router;
