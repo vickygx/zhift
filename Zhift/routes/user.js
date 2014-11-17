99b46f99fe8590afae88f19d51dbe48928039476
Dylan Joss <djoss@mit.edu>
2014-11-16 21:13:38 -0500

    clean up

--- Zhift/routes/user.js
+++ Zhift/routes/user.js
@@ -1,5 +1,15 @@
+/*  All routes relating to users 
+    
+    @author: Dylan Joss
+*/
+
 var express = require('express');
 var router = express.Router();
+
+// Controllers
+var OrgController = require('../controllers/organization');
+var errors = require('../errors/errors');
+var errorChecking = require('../errors/error-checking');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
