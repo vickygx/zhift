4c9f62110084f67e0035de01b3f49fef5cbf5011
Dylan Joss <djoss@mit.edu>
2014-11-18 22:06:38 -0500

    standardize JSDoc for most of controllers

--- Zhift/controllers/schedule.js
+++ Zhift/controllers/schedule.js
@@ -1,79 +1,76 @@
-/*  All the functions related to manipulating and retrieving information 
-    from the Schedule database
-
-    @author: Anji Ren, Dylan Joss
-*/
+/**  
+ * All the functions related to manipulating and retrieving information 
+ * from the Schedule database
+ *
+ * @author: Anji Ren, Dylan Joss
+ */
 var Schedule = require('../models/schedule');
 var errors = require('../errors/errors');
 module.exports = {};
 
-/*  Function to create a schedule
-    
-    @param 
-        {String} orgName:       name of organization
-        {String} role:          role for which schedule displays shifts for
-        {function} fn:          call back function
-
-    @return ---
-*/
+/**  
+ * Create a schedule
+ *   
+ * @param {String} orgName: name of organization
+ * @param {String} role: role for which schedule displays shifts
+ * @param {function} fn: callback function
+ *
+ * @return ---
+ */
 module.exports.createSchedule = function(orgName, role, fn) {
-    // Create new Schedule
     var schedule = new Schedule({
        org: orgName,
        role: role
     });
 
-    // Add to database
     schedule.save(fn);
 };
 
-/*  Function to retrieve a schedule
-    
-    @param 
-        {ObjectId} scheduleId:  id of the schedule to be retrieved
-        {function} fn:          callback function
-
-    @return ---
-*/
+/**  
+ * Retrieve a schedule, given its id
+ *   
+ * @param {ObjectId} scheduleId: id of the schedule to be retrieved
+ * @param {function} fn: callback function
+ *
+ * @return ---
+ */
 module.exports.retrieveSchedule = function(scheduleId, fn) {
     Schedule.findById(scheduleId, fn);
 }
 
-/*  Function to delete a schedule
-    
-    @param 
-        {ObjectId} scheduleId:  id of the schedule to be deleted
-        {function} fn:          callback function
-
-    @return ---
-*/
+/**  
+ * Delete a schedule
+ *   
+ * @param {ObjectId} scheduleId: id of the schedule to be deleted
+ * @param {function} fn: callback function
+ *
+ * @return ---
+ */
 module.exports.deleteSchedule = function(scheduleId, fn) {
     Schedule.findByIdAndRemove(scheduleId, fn);
 }
 
-/*  Function to retrieve all schedules associated with an organization
-    
-    @param 
-        {String} orgName:  name of organization
-        {function} fn:       callback function
-
-    @return ---
-*/
+/**  
+ * Retrieve all schedules associated with an organization
+ *   
+ * @param {String} orgName: name of organization
+ * @param {function} fn: callback function
+ *
+ * @return ---
+ */
 module.exports.retrieveSchedulesByOrg = function(orgName, fn) {
     Schedule.find({org: orgName}, fn);
 }
 
-/*  Function to retrieve the schedule associated with an organization and a role
-    
-    @param 
-        {String} orgName:  name of organization
-        {String} roleName: name of role
-        {function} fn:       callback function
-
-    @return ---
-*/
+/**  
+ * Retrieve the schedule associated with an organization and a role
+ *   
+ * @param {String} orgName: name of organization
+ * @param {String} roleName: name of role
+ * @param {function} fn: callback function
+ *
+ * @return ---
+ */
 module.exports.retrieveScheduleByOrgAndRole = function(orgName, roleName, fn) {
-    console.log(orgName);
-    console.log(roleName);
     Schedule.findOne({org: orgName, role: roleName}, fn);
 }
