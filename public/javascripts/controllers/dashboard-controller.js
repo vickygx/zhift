287ebd286e4bb34ed69c34daeaaaaeca96f3e1a5
Lily Seropian <lilyseropian@gmail.com>
2014-11-22 16:03:00 -0500

    Manager dashboard works

--- public/javascripts/controllers/dashboard-controller.js
+++ public/javascripts/controllers/dashboard-controller.js
@@ -10,22 +10,38 @@ var ZhiftApp = angular.module('ZhiftApp');
 
 ZhiftApp.controller('RecordController', function($scope, RecordService, ScheduleService) {
 
-    $scope.init = function(scheduleId) {
+    /**
+     * Get all records related to a given schedule and update the scope.
+     * @param  {String} scheduleId The id of the schedule to get records for.
+     * @param  {String} role       The name of the schedule.
+     */
+    var getRecordsForSchedule = function(scheduleId, role) {
+        RecordService.getRecordsFor(scheduleId, function(records) {
+            $scope.records.push({
+                role: role,
+                records: records,
+            });
+            $scope.$apply();
+        });
+    };
+
+    /**
+     * Put all the records on the scope.
+     * @param  {String} orgName    The name of the current user's organization.
+     * @param  {String} scheduleId The id of the current user's role, if the current user is an employee. Otherwise 'undefined'.
+     */
+    $scope.init = function(orgName, scheduleId) {
         $scope.records = [];
 
-        // is an employee
-        if (scheduleId !== undefined) {
-            console.log('is emp');
+        if (scheduleId !== 'undefined') { // is an employee
             ScheduleService.getSchedule(scheduleId, function(schedule) {
-                console.log('got sched');
-                RecordService.getRecordsFor(scheduleId, function(records) {
-                    console.log('got records', records);
-                    $scope.records.push({
-                        role: schedule.role,
-                        records: records,
-                    });
-                    console.log($scope);
-                    $scope.$apply();
+                getRecordsForSchedule(scheduleId, schedule.role);
+            });
+        }
+        else { // is a manager
+            ScheduleService.getSchedules(orgName, function(schedules) {
+                schedules.forEach(function(schedule) {
+                    getRecordsForSchedule(schedule._id, schedule.role);
                 });
             });
         }
