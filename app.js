cc0d8552b9c8b1bf9bb3ec664c49ee2b38ce5693
Lily Seropian <lilyseropian@gmail.com>
2014-11-27 17:03:13 -0500

    Set up testing files

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'tests')));
 app.use(flash());
 
 app.use('/', routes);
---
+++ tests/shift-tests.js
@@ -0,0 +1,3 @@
+QUnit.test('QUnit Test', function(assert) {
+    assert.equal(2, 2, 'Qunit works.');
+});
