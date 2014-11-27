c8f032aa8cae7ee61439c37e149952051bdcf686
Lily Seropian <lilyseropian@gmail.com>
2014-11-26 22:39:26 -0500

    Added preliminary error logging for cron

--- bin/weekly-maintenance.js
+++ bin/weekly-maintenance.js
@@ -1,14 +1,27 @@
 #!/usr/bin/env node
 
 var request = require('request');
+var fs = require('fs');
 
 var CRON_EMAIL = 'cron@zhift.com';
 var CRON_PASSWORD = '99570761084371110795';
 var CRON_ORG = 'Zhift';
 var URL = 'http://zhift-seropian.rhcloud.com';
+var LOG_FILE = process.env.OPENSHIFT_LOG_DIR;
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD === undefined) {
     URL = 'http://localhost:8080';
+    LOG_FILE = '../logs/weekly-maintenance.log';
+}
+
+var logError = function(err) {
+    if (err) {
+        fs.appendFile(LOG_FILE, new Date() + ': ' + err + '.\n', function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
 }
 
 /**
@@ -22,14 +35,17 @@ var loginBody = {
 };
 
 request.post(URL + '/login', {form: loginBody}, function(err, res, body) {
+    if (err) {
+        return logError(err);
+    }
+
     if (body === 'Moved Temporarily. Redirecting to /') {
-        console.log('FAILED TO LOG IN');
+        return logError('Failed to log in');
     }
-    else {
-        createNewShifts();
-        deleteAllOldShifts();
-        deleteAllOldRecords();
-    }
+
+    createNewShifts();
+    deleteAllOldShifts();
+    deleteAllOldRecords();
 });
 
 
@@ -40,13 +56,9 @@ var createNewShiftBody = {
     week: 3,
 }
 function createNewShifts() {
-    getAllTemplateShifts(function(err, templateShifts) {
+    getAllTemplateShifts(function(templateShifts) {
         templateShifts.forEach(function(templateShift) {
-            request.post(URL + '/shift/' + templateShift._id, {form: createNewShiftBody}, function(err, res, body) {
-                if (err) {
-                    console.log(err);
-                }
-            });
+            request.post(URL + '/shift/' + templateShift._id, {form: createNewShiftBody}, logError);
         });
     });
 };
@@ -57,7 +69,11 @@ function createNewShifts() {
  */
 function getAllTemplateShifts(fn) {
     request.get(URL + '/template/all', function (err, res, body) {
-        fn(null, JSON.parse(body));
+        body = JSON.parse(body);
+        if(body.message) {
+            return logError(body.message);
+        }
+        fn(body);
     });
 };
 
@@ -65,20 +81,12 @@ function getAllTemplateShifts(fn) {
  * Delete all shifts that have already occurred.
  */
 function deleteAllOldShifts() {
-    request.del(URL + '/shift/old', function (err, res, body) {
-        if (err) {
-            console.log(err);
-        }
-    });
+    request.del(URL + '/shift/old', logError);
 };
 
 /**
  * Delete all records pertaining to shifts that have already occurred.
  */
 function deleteAllOldRecords() {
-    request.del(URL + '/record/old', function (err, res, body) {
-        if (err) {
-            console.log(err);
-        }
-    });
+    request.del(URL + '/record/old', logError);
 };
