ded5ef27ae077e916ba45ed3eac51cc6a87c6c36
Lily Seropian <lilyseropian@gmail.com>
2014-12-01 11:49:06 -0500

    Skeleton for record tests

---
+++ tests/record-tests.js
@@ -0,0 +1,18 @@
+/**
+ * Tests for Record routes.
+ * @author Lily Seropian
+ */
+
+QUnit.module('Record');
+
+QUnit.asyncTest('GET', function(assert) {
+    
+});
+
+QUnit.asyncTest('GET schedule', function(assert) {
+    
+});
+
+QUnit.asyncTest('DELETE', function(assert) {
+    
+});
