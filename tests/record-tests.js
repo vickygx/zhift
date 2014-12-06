a688eb07769609851765972dea7f5c699092668c
Dylan Joss <djoss@mit.edu>
2014-12-06 17:31:35 -0500

    record tests now part of proper module in HTML test page

--- tests/record-tests.js
+++ tests/record-tests.js
@@ -46,6 +46,8 @@ function testRecordRoutes(data) {
     });
 
     var runDeleteTests = function() {
+        QUnit.module('Record');
+
         QUnit.asyncTest('DELETE', function(assert) {
             $.ajax('/record/old', {
                 type: 'DELETE',
